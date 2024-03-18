e8292a6e91903ab8371eba35dbef978f6afcdfa4
jozsef.huszko <jozsef.huszko@trendency.hu>
2024-03-18 12:38:21 +0100

    GTM2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
-import {ApplicationConfig} from '@angular/core'
+import {ApplicationConfig, importProvidersFrom} from '@angular/core'
 import {provideRouter} from '@angular/router'
 
 import {routes} from './app.routes'
+import {GoogleTagManagerModule} from 'angular-google-tag-manager'
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    {provide: 'googleTagManagerId', useValue: 'GTM-TSJX6CF4'}]
+    importProvidersFrom(GoogleTagManagerModule.forRoot({
+      id: 'GTM-TSJX6CF4',
+    }))
+  ]
 }
