2c2b209913e9d26b5301102a852188433edb9e4f
jozsef.huszko <jozsef.huszko@trendency.hu>
2024-03-18 12:58:10 +0100

    GTM5

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,20 +6,4 @@ import {NavigationEnd, Router} from '@angular/router'
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor(
-    private gtmService: GoogleTagManagerService,
-    private router: Router
-    ) {
-    this.router.events.forEach(item => {
-    if (item instanceof NavigationEnd) {
-      const gtmTag = {
-        event: 'page',
-        pageName: item.url
-      };
-
-      this.gtmService.pushTag(gtmTag);
-    }
-  });
-  }
 }
