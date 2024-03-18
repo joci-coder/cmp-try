3fec0f78ac78a816040a34696015b10b16b1ce2e
jozsef.huszko <jozsef.huszko@trendency.hu>
2024-03-18 14:35:03 +0100

    GTM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit{
     this.router.events.forEach(item => {
       if (item instanceof NavigationEnd) {
         const gtmTag = {
-          event: 'gtm.js',
+          event: 'page',
           pageName: item.url
         };
 
